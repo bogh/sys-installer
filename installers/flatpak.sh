14ef8319204e00d46c932fc2bd08c74abef08c2b
Bogdan I. Bursuc <bogdanbursuc86@gmail.com>
2022-05-07 14:19:00 +0300

    Fixes

--- installers/flatpak.sh
+++ installers/flatpak.sh
@@ -5,7 +5,7 @@ sudo apt install gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 flatpak install -y flathub com.slack.Slack
-flatpak install -y flathub com.visualstudio.code
+# flatpak install -y flathub com.visualstudio.code
 flatpak install -y flathub com.dropbox.Client
 flatpak install -y flathub info.smplayer.SMPlayer
 flatpak install -y flathub com.github.hluk.copyq
