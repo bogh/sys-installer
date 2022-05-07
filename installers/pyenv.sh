272c2a1a4df1a31c96feda520c34e5dcec3f14ef
Bogdan I. Bursuc <bogdanbursuc86@gmail.com>
2022-05-07 15:21:12 +0300

    Install python3-venv

--- installers/pyenv.sh
+++ installers/pyenv.sh
@@ -2,7 +2,8 @@
 
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
     libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
-    libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
+    libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl \
+    python3-venv
 
 curl https://pyenv.run | bash
 
