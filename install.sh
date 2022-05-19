d7f2f260b5b5022f57cb3034b5ebae30dadbe814
Bogdan I. Bursuc <bogdanbursuc86@gmail.com>
2022-05-19 09:34:53 +0300

    update

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+
 set -ex
+
 sudo apt-get update
 sudo apt-get install -y
 sudo apt-get install -y aptitude vim git gitk mc neovim fish terminator \
     gnome-tweaks build-essential \
-    barrier fish
+    barrier fish \
+    python3-venv
 
 # Libjpeg support, for Pillow mainly
 sudo apt-get install -y libjpeg-dev libfreetype6 libfreetype6-dev zlib1g-dev
 sudo apt-get install -y libxml2-dev libxslt1-dev libexif-dev exiftool libmagickwand-dev python3-exif
-# apt-get install -y libmysqlclient-dev postgresql-client
-sudo apt-get install -y postgresql-server-dev-all
+# db dev libs
+sudo apt-get install -y postgresql-server-dev-all libmysqlclient-dev
 
 # aptitude install -y nodejs npm
 
@@ -18,7 +21,7 @@ sudo apt-get install -y python3-pip python3-dev
 
 pip3 install pipenv
 
-./install/code.sh
+./installers/code.sh
 ./installers/docker.sh
 ./installers/flatpak.sh
 ./installers/fonts.sh
