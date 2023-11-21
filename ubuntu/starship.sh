5555ce33622fbfd4ed8b847af6808a01487684d1
Bogdan I. Bursuc <bogdanbursuc86@gmail.com>
2023-11-21 12:21:22 +0200

    Update

--- ubuntu/starship.sh
+++ ubuntu/starship.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+apt-get install -y aptitude
 aptitude install fish
 
 curl -sS https://starship.rs/install.sh | sh
@@ -7,4 +8,6 @@ curl -sS https://starship.rs/install.sh | sh
 mkdir -p $HOME/.config/fish
 printf "\n\nstarship init fish | source" >> $HOME/.config/fish/config.fish
 
-ln -s $(realpath ../configs/starship.toml) $HOME/.config/
+cp $(realpath ./starship.toml) $HOME/.config/startship.toml
+
+# raw github file: https://raw.githubusercontent.com/bogh/sys-installer/fa6e00e5afaa6c72a1c49bde1d766ec1810916db/ubuntu/starship.sh
